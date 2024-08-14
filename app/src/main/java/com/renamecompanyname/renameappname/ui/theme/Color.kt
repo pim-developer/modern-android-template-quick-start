ff32aeee33429acc0e79eb20a742322e246d09c3
Pim Dhaen <dhaen.dev@gmail.com>
2024-08-14 17:08:49 +0200

    add shades of grey to colors

--- app/src/main/java/com/renamecompanyname/renameappname/ui/theme/Color.kt
+++ app/src/main/java/com/renamecompanyname/renameappname/ui/theme/Color.kt
@@ -5,10 +5,16 @@ import androidx.compose.ui.graphics.Color
 // Generate your Material 3 color scheme using the official figma plugin:
 // https://www.figma.com/community/plugin/1034969338659738588/material-theme-builder
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// If you haven't decided on a color scheme yet, you can
+// use these colors as placeholders to visualize color hierarchy.
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Grey10 = Color(0xFFFAFAFA) // Very light grey
+val Grey20 = Color(0xFFF0F0F0) // Light grey
+val Grey30 = Color(0xFFD9D9D9) // Lighter grey
+val Grey40 = Color(0xFFBFBFBF) // Light-medium grey
+val Grey50 = Color(0xFFA6A6A6) // Medium grey
+val Grey60 = Color(0xFF8C8C8C) // Dark-medium grey
+val Grey70 = Color(0xFF737373) // Dark grey
+val Grey80 = Color(0xFF595959) // Darker grey
+val Grey90 = Color(0xFF404040) // Very dark grey
+val Grey100 = Color(0xFF262626) // Almost black
